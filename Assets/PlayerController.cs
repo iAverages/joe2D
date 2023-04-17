58eb913c863e5c700b418c0e9c8f8000da970647
iAverages <31937542+iAverages@users.noreply.github.com>
2023-04-17 20:50:59 +0100

    walking & idle animations

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -11,12 +11,16 @@ public class PlayerController : MonoBehaviour
     
     Vector2 movementInput;
     Rigidbody2D rb;
+    Animator animator;
+    SpriteRenderer spriteRenderer;
     List<RaycastHit2D> castCollision = new List<RaycastHit2D>();
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
@@ -26,10 +30,12 @@ public class PlayerController : MonoBehaviour
     }
 
     private void FixedUpdate() {
+        bool moved = false;
+
         // If movement input is not 0, dont move.
         if (movementInput != Vector2.zero) {
             // Try move the player in the direction they want.
-            bool moved = TryMove(movementInput);
+            moved = TryMove(movementInput);
             // If they are not able to move, test x and y separately. 
             // This allows for a player to move up a wall if they are
             // right up against it.
@@ -38,14 +44,18 @@ public class PlayerController : MonoBehaviour
                 moved = TryMove(new Vector2(movementInput.x, 0));
                 if (!moved) {
                     // Try for Y
-                    TryMove(new Vector2(0, movementInput.y));
+                    moved = TryMove(new Vector2(0, movementInput.y));
                 }
             }
         }
+        animator.SetBool("IsMoving", moved);
+        spriteRenderer.flipX = movementInput.x < 0;
     }
 
 
     private bool TryMove(Vector2 direction) {
+        if (direction == Vector2.zero) return false;
+        
         int count = rb.Cast(
                 direction,
                 movementFilter,
