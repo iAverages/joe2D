5be36e249580c4615b43b88f9d505df71e55cb20
dan <31937542+iAverages@users.noreply.github.com>
2023-04-20 23:02:59 +0100

    Added Pause menu (#14)
    * this is better use this branch
    
    * main menu working
    
    * background fixes
    
    * better menu and working quit in game
    
    * fucked
    
    * pause button works
    
    * better pause ui
    
    * player will disapper on pause
    
    * works a little too well
    
    * button hover
    
    * Fix invis player on pause + sizing + cleanup
    
    ---------
    
    Co-authored-by: archiet123 <114408893+archiet123@users.noreply.github.com>

---
+++ Assets/Scripts/PauseGameController.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseGameController : MonoBehaviour {     
+    public static bool isPaused = false;
+    public GameObject pauseMenu;
+    public GameObject player;
+
+    void Start() {        
+        isPaused = false;
+        pauseMenu.SetActive(false);
+    }
+
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.Escape)) {
+            if (isPaused) {
+                ResumeGame();        
+            } else {
+                PauseGame();
+            }
+        }
+    }
+
+    public void PauseGame() {
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0f;
+        isPaused = true;
+        player.SetActive(false);
+    }
+
+    public void ResumeGame() {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1f;
+        isPaused = false;
+        player.SetActive(true);
+    }   
+}
+
