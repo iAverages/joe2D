f2c7b50e4d97b2d55bd2b807612d775d851e9970
iAverages <31937542+iAverages@users.noreply.github.com>
2023-04-21 16:08:40 +0100

    Cleanup

--- Assets/Characters/Player/SoundController.cs
+++ Assets/Characters/Player/SoundController.cs
@@ -2,18 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class testing : MonoBehaviour
+public class SoundController : MonoBehaviour
 {
     [SerializeField] AudioClip[] sounds; 
-    AudioSource myAudioSource;
+    AudioSource audioSource;
 
-    // Start is called before the first frame update
     void Start()
     {
-        myAudioSource = GetComponent<AudioSource>();
+        audioSource = GetComponent<AudioSource>();
     }
 
-    // Update is called once per frame
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.F))
@@ -26,7 +24,6 @@ public class testing : MonoBehaviour
     public void PlayRandomSound()
     {
         AudioClip clip = sounds[UnityEngine.Random.Range(0, sounds.Length)];
-        myAudioSource.PlayOneShot(clip);
-        // Debug.Log("worked");
+        audioSource.PlayOneShot(clip);
     }
 }
