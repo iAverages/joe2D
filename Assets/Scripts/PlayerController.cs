326e59baf6e8358114917df18c1bb6245c94b496
archiet123 <114408893+archiet123@users.noreply.github.com>
2023-04-21 14:53:14 +0100

    works with 1 mp3 file needs randomising

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -13,37 +13,20 @@ public class PlayerController : MonoBehaviour
     Rigidbody2D rb;
     Animator animator;
     SpriteRenderer spriteRenderer;
-    List<RaycastHit2D> castCollision = new List<RaycastHit2D>();
-
-    public AudioSource[] collisionSounds;
-
-    public AudioSource collision1;
-    public AudioSource collision2;
-    public AudioSource collision3;
-    public AudioSource collision4;
+    List<RaycastHit2D> castCollision = new List<RaycastHit2D>(); 
+    
 
     // Start is called before the first frame update
     void Start() {
         rb = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
-        spriteRenderer = GetComponent<SpriteRenderer>();
-
-
-        collisionSounds = GetComponents<AudioSource>();
-        collision1 = collisionSounds[0];
-        collision2 = collisionSounds[1];
-        collision3 = collisionSounds[2];
-        collision4 = collisionSounds[3];
+        spriteRenderer = GetComponent<SpriteRenderer>();        
     }
 
     // Update is called once per frame
-    void Update() 
-    {
-        if (Input.GetKeyDown("f"))
-        {     
-            PlayRandomSound();
-        }
-    }
+    void Update() {}
+
+    
 
     private void FixedUpdate() {
         bool moved = false;
@@ -91,10 +74,7 @@ public class PlayerController : MonoBehaviour
         movementInput = movementValue.Get<Vector2>();
     }
 
-    public void PlayRandomSound()
-        { 
-            collisionSounds[Random.Range(0, collisionSounds.Length)].Play();
-        }
+    
 }
 
 
---
+++ Assets/fartScript.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fartScript : MonoBehaviour
+{
+
+    public AudioSource fart;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.F))
+        {     
+            PlayRandomSound();
+            Debug.Log("F key was pressed.");
+        }
+    }
+
+    public void PlayRandomSound(){
+        fart.Play();
+    }
+}
