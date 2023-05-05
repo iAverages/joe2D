56d452fe94284d9b31c730295d62b54c5ee6e874
iAverages <31937542+iAverages@users.noreply.github.com>
2023-05-05 14:16:13 +0100

    Format files I opened

--- Assets/Scripts/Utilities/DeveloperConsole/DeveloperConsoleBehaviour.cs
+++ Assets/Scripts/Utilities/DeveloperConsole/DeveloperConsoleBehaviour.cs
@@ -6,27 +6,36 @@ using TMPro;
 
 public class DeveloperConsoleBehaviour : MonoBehaviour
 {
-    [SerializeField] private ConsoleCommand[] commands = new ConsoleCommand[0];
+    [SerializeField]
+    private ConsoleCommand[] commands = new ConsoleCommand[0];
 
     [Header("UI")]
-    [SerializeField] private GameObject uiCanvas = null;
-    [SerializeField] private TMP_InputField inputField = null;
+    [SerializeField]
+    private GameObject uiCanvas = null;
 
-    private float pausedTimeScale; 
+    [SerializeField]
+    private TMP_InputField inputField = null;
+
+    private float pausedTimeScale;
 
     private static DeveloperConsoleBehaviour instance;
 
     // Singlton for dev console
     private DeveloperConsole developerConsole;
-    private DeveloperConsole DeveloperConsole {
-        get {
-            if (developerConsole != null) return developerConsole;
+    private DeveloperConsole DeveloperConsole
+    {
+        get
+        {
+            if (developerConsole != null)
+                return developerConsole;
             return developerConsole = new DeveloperConsole(commands);
         }
     }
 
-    private void Awake() {
-        if (instance != null && instance != this) {
+    private void Awake()
+    {
+        if (instance != null && instance != this)
+        {
             Destroy(gameObject);
             return;
         }
@@ -36,13 +45,18 @@ public class DeveloperConsoleBehaviour : MonoBehaviour
         DontDestroyOnLoad(gameObject);
     }
 
-    public void Toggle(CallbackContext context) {
-        if (!context.action.triggered) return;
+    public void Toggle(CallbackContext context)
+    {
+        if (!context.action.triggered)
+            return;
 
-        if (uiCanvas.activeSelf) {
+        if (uiCanvas.activeSelf)
+        {
             Time.timeScale = pausedTimeScale;
             uiCanvas.SetActive(false);
-        } else {
+        }
+        else
+        {
             pausedTimeScale = Time.timeScale;
             Time.timeScale = 0;
             uiCanvas.SetActive(true);
@@ -50,7 +64,8 @@ public class DeveloperConsoleBehaviour : MonoBehaviour
         }
     }
 
-    public void ProcessCommand(string input) {
+    public void ProcessCommand(string input)
+    {
         DeveloperConsole.handleCommand(input);
         inputField.text = string.Empty;
     }
