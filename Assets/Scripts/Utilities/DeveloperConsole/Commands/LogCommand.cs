4d4e30a19119be9ba6b653c43cea29908880acdc
iAverages <31937542+iAverages@users.noreply.github.com>
2023-05-06 17:32:32 +0100

    Some stuff idk lol

--- Assets/Scripts/Utilities/DeveloperConsole/Commands/LogCommand.cs
+++ Assets/Scripts/Utilities/DeveloperConsole/Commands/LogCommand.cs
@@ -1,8 +1,11 @@
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "New Log Command", menuName = "Utilities/DeveloperConsole/Commands/Log Command")]
-public class LogCommnad : ConsoleCommand {
-
+[CreateAssetMenu(
+    fileName = "New Log Command",
+    menuName = "Utilities/DeveloperConsole/Commands/Log Command"
+)]
+public class LogCommand : ConsoleCommand
+{
     public override bool handle(string[] args)
     {
         Debug.Log(string.Join(" ", args));
