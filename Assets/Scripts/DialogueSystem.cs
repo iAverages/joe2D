1940bcc6eb36150b25e07afa2e3de1d2959de0de
archiet123 <114408893+archiet123@users.noreply.github.com>
2023-05-07 01:40:42 +0100

    sentences are working, hitboxes are arse

--- Assets/Scripts/DialogueSystem.cs
+++ Assets/Scripts/DialogueSystem.cs
@@ -15,6 +15,31 @@ public class DialogueSystem : MonoBehaviour
     public void StartDialogue(Dialogue dialogue)
     {
         Debug.Log("started conversation with " + dialogue.npc_name);
+
+        sentences.Clear();
+
+        foreach (string sentence in dialogue.sentences)
+        {
+            sentences.Enqueue(sentence);
+        }
+
+        DisplayNextSentence();        
     }
     
+    public void DisplayNextSentence()
+    {
+        if (sentences.Count == 0)
+        {
+            EndDialogue();
+            return;
+        }
+
+        string sentence = sentences.Dequeue();
+        Debug.Log(sentence);
+    }
+
+    void EndDialogue()
+    {
+        Debug.Log("End");
+    }
 }
