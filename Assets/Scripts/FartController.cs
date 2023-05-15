49eba1736d5cee7408770d2a79e6595850e620f6
callum <114409010+callum-mac@users.noreply.github.com>
2023-05-15 19:05:32 +0100

    added fart animation (#57)
    * added fart animation
    
    * Add fart animation
    
    ---------
    
    Co-authored-by: dan <31937542+iAverages@users.noreply.github.com>

--- Assets/Scripts/FartController.cs
+++ Assets/Scripts/FartController.cs
@@ -4,12 +4,15 @@ using UnityEngine;
 
 public class FartController : MonoBehaviour
 {
-    [SerializeField] AudioClip[] sounds;
+    [SerializeField]
+    AudioClip[] sounds;
     AudioSource audioSource;
+    Animator animator;
 
     void Start()
     {
         audioSource = GetComponent<AudioSource>();
+        animator = GetComponent<Animator>();
     }
 
     void Update()
@@ -17,7 +20,20 @@ public class FartController : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.F))
         {
             PlayRandomSound();
+        }
 
+        AnimatorStateInfo animatorStateInfo = animator.GetCurrentAnimatorStateInfo(0);
+        // Check if fart animation is running
+        if (animator.GetCurrentAnimatorStateInfo(0).IsName("player_fart"))
+        {
+            float animationTime = animatorStateInfo.normalizedTime;
+
+            // If animationTime is more than 1 then the animation has finsihed running
+            // (Animation does not loop)
+            if (animationTime > 1.0f)
+            {
+                animator.Play("player_idle");
+            }
         }
     }
 
@@ -25,5 +41,6 @@ public class FartController : MonoBehaviour
     {
         AudioClip clip = sounds[UnityEngine.Random.Range(0, sounds.Length)];
         audioSource.PlayOneShot(clip);
+        animator.Play("player_fart");
     }
 }
